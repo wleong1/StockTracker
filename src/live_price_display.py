cf9bf882899ebe4ea28b7387dab93a3adf45b2b6
wleong1 <weijieleong88@gmail.com>
2023-12-02 03:01:22 +0000

    Recommitted old file

--- src/live_price_display.py
+++ src/live_price_display.py
@@ -3,8 +3,13 @@ from PyQt5.QtGui import QFont
 from PyQt5 import QtCore
 import requests
 
+<<<<<<< HEAD
 from data_processing import DataProcessing
 from parameters import ALPHA_VANTAGE_API_KEY
+=======
+from processing.data_processing import DataProcessing
+from parameters import ALPHA_VANTAGE_API_KEY, mongodb_connection
+>>>>>>> a55d65f (Removed parameters)
 
 ALPHA_VANTAGE_ENDPOINT = "https://www.alphavantage.co/query"
 
@@ -48,3 +53,28 @@ class LivePriceDisplay(QWidget):
             self.price: str = "Error fetching price"
 
         self.share_price_label.setText(f"{company_name}:\n{self.price}")
+<<<<<<< HEAD
+=======
+
+from pymongo import MongoClient
+client = MongoClient(mongodb_connection)
+database = client.StockTracker
+collection = database.Companies
+projection = {"_id": 0, "name": 1, "price": 1}
+# cursor = collection.find({"name": "MSFT"}, projection)
+# for doc in cursor:
+#     latest_date = doc["price"][0]["date"]
+# print(latest_date)
+symbols = ["AAPL", "MSFT", "AMZN", "GOOGL", "NVDA"]# "TSLA", "GOOG", "BRK.B", "META", "UNH"
+for symbol in symbols:
+    price_params: dict = {
+        "apikey": ALPHA_VANTAGE_API_KEY,
+        "function": "TIME_SERIES_DAILY",
+        "symbol": symbol,
+        "outputsize": "full"
+    }
+    a = requests.get(ALPHA_VANTAGE_ENDPOINT, params=price_params).json()
+    company = {"_id": symbol, "price":[{"date": b, "close": a["Time Series (Daily)"][b]["4. close"]} for b in a["Time Series (Daily)"]]}
+    result = collection.insert_one(company)
+    print(f"Inserted document ID: {result.inserted_id}")
+>>>>>>> a55d65f (Removed parameters)

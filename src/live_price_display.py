fcf8a3218c0f912066cc5d942d3270cc1896c85b
wleong1 <weijieleong88@gmail.com>
2024-03-03 17:42:50 +0000

    Added more tests

--- src/live_price_display.py
+++ src/live_price_display.py
@@ -79,7 +79,6 @@ class LivePriceDisplay:
         except IndexError:
             return "Error fetching price"
 
-
 # from pymongo import MongoClient
 # client = MongoClient(mongodb_connection)
 # database = client.StockTracker

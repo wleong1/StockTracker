4468f09758d3834fbf52607f339a3fc75cb6af61
wleong1 <weijieleong88@gmail.com>
2024-03-06 23:53:21 +0000

    Added django dashboard and tests

--- src/live_price_display.py
+++ src/live_price_display.py
@@ -79,6 +79,7 @@ class LivePriceDisplay:
         except IndexError:
             return "Error fetching price"
 
+
 # from pymongo import MongoClient
 # client = MongoClient(mongodb_connection)
 # database = client.StockTracker

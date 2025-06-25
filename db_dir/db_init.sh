83ccaabf666b51ccb38162120c9fec457fb0b681
Wei Jie Leong <122815453+wleong1@users.noreply.github.com>
2025-06-25 02:18:35 +0100

    Orm integration (#24)
    * Changed to ORM
    
    * Added pgbouncer and containerised pgbouncer
    
    * Removed nfs
    
    * Added sql file with LFS
    
    * Refactoring

--- db_dir/db_init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Creates an empty stocks database
-psql -U postgres -c "CREATE DATABASE stocks;"
-
-# Restore the data into the stocks database
-pg_restore -U postgres -d stocks /nfs/db.dump
